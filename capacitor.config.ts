06673ed4a5b11ee3bf01ff348187f2c614e7db5c
pkm-sync <sync@houseofmates.space>
2026-05-11 11:07:12 -0700

    autocommit 11:07:12

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'space.houseofmates.torrent',
-  appName: 'torrent',
-  webDir: 'build'
+	appId: 'space.houseofmates.torrent',
+	appName: 'torrent',
+	webDir: 'build',
+	server: {
+		url: 'https://tor.houseofmates.space',
+		cleartext: false
+	}
 };
 
 export default config;

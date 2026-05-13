0cef2a023eed8d0dcc924f9bf380a5913fdfea2a
pkm-sync <sync@houseofmates.space>
2026-05-12 17:28:09 -0700

    autocommit 17:28:09

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
+		port: 3004,
+		host: '0.0.0.0',
 		allowedHosts: ['tor.houseofmates.space', '.houseofmates.space']
 	}
 });
